049e136c859aa8e49c63f49f29e6683c3c960b59
pt74847781 <ariel_sc_2001@hotmail.com>
2023-06-19 11:18:26 -0500

    first commit

---
+++ src/main/java/com/example/idatsw1lsrestsaludp/IdatSw1LsRestSaludpApplication.java
@@ -0,0 +1,13 @@
+package com.example.idatsw1lsrestsaludp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdatSw1LsRestSaludpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdatSw1LsRestSaludpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/idatsw1lsrestsaludp/IdatSw1LsRestSaludpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.idatsw1lsrestsaludp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdatSw1LsRestSaludpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
